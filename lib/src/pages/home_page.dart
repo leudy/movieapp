635ebe3a5f4fd911e4332859fa7032e3b1343c34
Leudi Rosario <leudi_rosario@hotmail.com>
2019-06-04 18:47:12 -0400

    setting-up-project

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Peliculas en cines'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: SafeArea(
+          child: Text('Hola Mundo'),
+        ));
+  }
+}
