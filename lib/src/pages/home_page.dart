5ad80fe05975a89244bda9f1b7a83c3d9a486b1c
Leudi Rosario <leudi_rosario@hotmail.com>
2019-06-08 17:56:26 -0400

    adding-swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 
 class HomePage extends StatelessWidget {
   @override
@@ -14,8 +16,26 @@ class HomePage extends StatelessWidget {
             )
           ],
         ),
-        body: SafeArea(
-          child: Text('Hola Mundo'),
-        ));
+        body: Container(child: Column(children: <Widget>[
+          _CrearTarjetas()
+        ],),);
+  }
+
+  Widget _CrearTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: new Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 250.0,
+        itemBuilder: (BuildContext context,int index){
+          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
+        },
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+      ),
+    );
   }
 }
